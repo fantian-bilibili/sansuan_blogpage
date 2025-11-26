12115fd13c849a74a942b0f306f45434b9952f47
SanSuan <Yu3189274087@outlook.com>
2025-11-26 21:33:11 +0800

    fix

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -1,8 +1,9 @@
 "use client";
 
+import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+// 使用 React.ComponentProps 自动获取组件的类型，这样就不会报错了
+export function ThemeProvider({ children, ...props }: React.ComponentProps<typeof NextThemesProvider>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
